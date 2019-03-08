50fa5c06bc565bca96f236c1b8f68d50c0ebc362
mzy <bryate_mzy@163.com>
2019-03-08 20:32:15 +0800

    3.8.2

--- GeoProblemSolving-front/src/router/index.js
+++ GeoProblemSolving-front/src/router/index.js
@@ -23,7 +23,6 @@ import projectList from '@/components/Projects/ProjectList.vue'
 Vue.use(Router)
 Vue.use(mavonEditor)
 
-import ModuleDemo from '@/components/utils/ModuleDemo.vue'  //module的WebSocket测试
 
 const routes = [{
   path: '/', name: 'Navigation', component: Navigation, children: [
