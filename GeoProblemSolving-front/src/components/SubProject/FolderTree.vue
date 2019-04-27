9570d8ed06c0e884a5bc524e2d492582814d5ad0
Bosh0113 <45487945+Bosh0113@users.noreply.github.com>
2019-04-27 11:04:34 +0800

    文件夹树：文件夹重命名

--- GeoProblemSolving-front/src/components/SubProject/FolderTree.vue
+++ GeoProblemSolving-front/src/components/SubProject/FolderTree.vue
@@ -10,6 +10,18 @@
     padding: 5px;
     margin: 3px;
 }
+.folderDeleteBtn{
+    cursor:pointer;
+    color:red;
+    float:right;
+    margin: 0 5px;
+}
+.folderRenameBtn{
+    cursor:pointer;
+    color:blue;
+    float:right;
+    margin: 0 5px;
+}
 </style>
 <template>
     <div class="fileSpace">
@@ -19,12 +31,27 @@
             <div v-show="currentFolder!='{}'">
                 <div v-for="(folder,index) in currentFolder.folders" :key="index">
                     <a @click="enterFolder(folder)" class="folderItem">{{folder.name}}</a>
-                    <span @click="deleteFolder(folder)" style="cursor:pointer;color:red;float:right">x</span>
+                    <span @click="deleteFolder(folder)" class="folderDeleteBtn">x</span>
+                    <span @click="renameFolderModalShow(folder)" class="folderRenameBtn">=</span>
                 </div>
                 <p v-for="(file,index) in currentFolder.files" @click="getFileInfo(file)">{{file.name}}</p>
             </div>
         </div>
         <Input v-model="newFolderName" placeholder="The name of new folder" style="width: 200px"/><Button @click="addFolder">newFolder</Button>
+        <Modal
+            v-model="renameFolderModal"
+            title="Rename folder"
+        >
+            <Form ref="applyValidate" :model="applyValidate" :rules="applyRuleValidate" :label-width="80">
+                <FormItem label="New name" prop="newName">
+                    <Input v-model="applyValidate.newName" type="textarea" :rows="4" placeholder="Enter the name for folder..." />
+                </FormItem>
+            </Form>
+            <div slot="footer">
+                <Button @click="renameFolderModal=false">Cancel</Button>
+                <Button type="success" @click="renameFolder('applyValidate')">Rename</Button>
+            </div>
+        </Modal>
     </div>
 </template>
 <script>
@@ -37,8 +64,21 @@ export default {
             subProjectFileStruct:{},
             subProjectId:"fbd438e0-b23b-49f6-93c8-36b678c70720",
             currentFolder:{},
-            folderStack:[],
+            folderUIDStack:[],
             newFolderName:"",
+            renameFolderInfo:{},
+            renameFolderModal:false,
+            applyValidate:{
+                newName:""
+            },applyRuleValidate: {
+                newName: [
+                {
+                    required: true,
+                    message: "The name can't be null.",
+                    trigger: "blur"
+                }
+                ]
+            },
         }
     },
     methods:{
@@ -56,12 +96,12 @@ export default {
             });
         },
         enterFolder(folder){
-            this.folderStack.push(this.currentFolder.uid);
+            this.folderUIDStack.push(this.currentFolder.uid);
             this.currentFolder = folder;
         },
         backforeFolder(){
-            if(this.folderStack.length>1){
-                var foreFolderUid= this.folderStack.pop();
+            if(this.folderUIDStack.length>1){
+                var foreFolderUid= this.folderUIDStack.pop();
                 this.refreshCurrentFolder(this.subProjectFileStruct,foreFolderUid);
             }else{
                 confirm("no fore folder.");
@@ -123,6 +163,37 @@ export default {
             })
             .catch(err=>{
                 console.log("Delete folder fail.");
+            });
+        },
+        renameFolderModalShow(folder){
+            this.renameFolderInfo = folder;
+            this.applyValidate.newName = "";
+            this.renameFolderModal = true;
+        },
+        renameFolder(name){
+            this.$refs[name].validate(valid => {
+                if (valid) {
+                    var currentFolderUid = this.currentFolder.uid;
+                    var subProjectId = this.subProjectId;
+                    var oldFolderInfo = this.renameFolderInfo;
+                    var newName = this.applyValidate.newName;
+                    this.axios
+                    .post("/GeoProblemSolving/subProject/renameFolder"+
+                    "?subProjectId="+subProjectId+
+                    "&parentId="+currentFolderUid+
+                    "&folderName="+newName+
+                    "&folderUid="+oldFolderInfo.uid)
+                    .then(res=>{
+                        if(res.data!="Fail"){
+                            this.subProjectFileStruct = res.data;
+                            this.refreshCurrentFolder(res.data,this.currentFolder.uid);
+                        }else{
+                            console.log("Rename fail.")
+                        }
+                    })
+                    .catch(err=>{console.log("Rename fail.")});
+                    this.renameFolderModal = false;
+                }
             });
         }
     }
