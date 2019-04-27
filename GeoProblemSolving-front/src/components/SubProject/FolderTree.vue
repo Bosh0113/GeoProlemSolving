748d43a2aa41953a344fad624fb18abbb0603d22
Bosh0113 <45487945+Bosh0113@users.noreply.github.com>
2019-04-27 14:47:58 +0800

    文件树：上传Modal

--- GeoProblemSolving-front/src/components/SubProject/FolderTree.vue
+++ GeoProblemSolving-front/src/components/SubProject/FolderTree.vue
@@ -1,26 +1,26 @@
 <style scoped>
-.fileSpace{
-    padding:100px
+.fileSpace {
+  padding: 100px;
 }
-.folderContent{
-    width: 300px;
-    height: 300px;
-    overflow-y: auto;
-    border: 0.3px yellowgreen solid;
-    padding: 5px;
-    margin: 3px;
+.folderContent {
+  width: 300px;
+  height: 300px;
+  overflow-y: auto;
+  border: 0.3px yellowgreen solid;
+  padding: 5px;
+  margin: 3px;
 }
-.folderDeleteBtn{
-    cursor:pointer;
-    color:red;
-    float:right;
-    margin: 0 5px;
+.folderDeleteBtn {
+  cursor: pointer;
+  color: red;
+  float: right;
+  margin: 0 5px;
 }
-.folderRenameBtn{
-    cursor:pointer;
-    color:blue;
-    float:right;
-    margin: 0 5px;
+.folderRenameBtn {
+  cursor: pointer;
+  color: blue;
+  float: right;
+  margin: 0 5px;
 }
 </style>
 <template>
@@ -38,13 +38,14 @@
             </div>
         </div>
         <Button @click="addFolderModalShow">newFolder</Button>
+        <Button @click="uploadModalShow" title="upload resource"><Icon type="md-cloud-upload" size="20"/></Button>
         <Modal
             v-model="renameFolderModal"
             title="Rename folder"
         >
             <Form ref="renameValidate" :model="renameValidate" :rules="renameRuleValidate" :label-width="80">
                 <FormItem label="New name" prop="newName">
-                    <Input v-model="renameValidate.newName" type="textarea" :rows="4" placeholder="Enter the name for folder..." />
+                    <Input v-model="renameValidate.newName" :rows="4" placeholder="Enter the name for folder..." />
                 </FormItem>
             </Form>
             <div slot="footer">
@@ -58,7 +59,7 @@
         >
             <Form ref="newValidate" :model="newValidate" :rules="newRuleValidate" :label-width="80">
                 <FormItem label="Set name" prop="setName">
-                    <Input v-model="newValidate.setName" type="textarea" :rows="4" placeholder="Enter the name for folder..." />
+                    <Input v-model="newValidate.setName" :rows="4" placeholder="Enter the name for folder..." />
                 </FormItem>
             </Form>
             <div slot="footer">
@@ -66,174 +67,319 @@
                 <Button type="success" @click="addFolder('newValidate')">New</Button>
             </div>
         </Modal>
+        <Modal v-model="uploadModal" title="Upload file" width="600">
+            <Form ref="uploadValidate" :model="uploadValidate" :rules="uploadRuleValidate" :label-width="100"  label-position="left" >
+                <FormItem label="Privacy" prop="privacy">
+                    <RadioGroup v-model="uploadValidate.privacy" style="width:80%">
+                        <Radio label="private">Private</Radio>
+                        <Radio label="public">Public</Radio>
+                    </RadioGroup>
+                </FormItem>
+                <FormItem label="Type" prop="type">
+                    <RadioGroup v-model="uploadValidate.type">
+                        <Radio label="data"></Radio>
+                        <Radio label="paper"></Radio>
+                        <Radio label="document"></Radio>
+                        <Radio label="model"></Radio>
+                        <Radio label="image"></Radio>
+                        <Radio label="video"></Radio>
+                        <Radio label="others"></Radio>
+                    </RadioGroup>
+                </FormItem>
+                <FormItem label="Description" prop="description">
+                    <Input
+                        type="textarea"
+                        :rows="4"
+                        v-model="uploadValidate.description"
+                    />
+                </FormItem>
+            </Form>
+            <Upload
+                :max-size="1024*1024"
+                multiple
+                type="drag"
+                :before-upload="gatherFile"
+                action="-"
+                >
+                <div style="padding: 20px 0">
+                    <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+                    <p>Click or drag file here to upload</p>
+                </div>
+            </Upload>
+            <div style="padding:0 10px 0 10px">
+                <ul v-for="(list,index) in toUploadFiles" :key="index">
+                    <li style="display:flex">
+                    filename:
+                    <span style="font-size:10px;width:60%">{{ list.name }}</span>
+                    size:
+                    <span
+                        style="font-size:10px;width:70%"
+                    >{{list.fileSize}}</span>
+                    <Icon
+                        type="ios-close"
+                        size="20"
+                        @click="delFileList(index)"
+                        style="display:flex;justify-content:flex-end;cursor:pointer"
+                    ></Icon>
+                    </li>
+                </ul>
+            </div>
+            <div slot="footer">
+                <Button @click="uploadModal=false">Cancel</Button>
+                <Button type="success" @click="subProjectUpload('uploadValidate')">Upload</Button>
+            </div>
+        </Modal>
     </div>
 </template>
 <script>
 export default {
-    created(){
-        this.getSubProjectInfo();
+  created() {
+    this.getSubProjectInfo();
+  },
+  data() {
+    return {
+      subProjectFileStruct: {},
+      subProjectId: "fbd438e0-b23b-49f6-93c8-36b678c70720",
+      currentFolder: {},
+      folderUIDStack: [],
+      newFolderModal: false,
+      setFolderName: "",
+      newValidate: {
+        setName: ""
+      },
+      newRuleValidate: {
+        setName: [
+          {
+            required: true,
+            message: "The name can't be null.",
+            trigger: "blur"
+          }
+        ]
+      },
+      renameFolderInfo: {},
+      renameFolderModal: false,
+      renameValidate: {
+        newName: ""
+      },
+      renameRuleValidate: {
+        newName: [
+          {
+            required: true,
+            message: "The name can't be null.",
+            trigger: "blur"
+          }
+        ]
+      },
+      uploadModal: false,
+      uploadValidate: {
+        privacy: "private",
+        type: "data",
+        description: ""
+      },
+      uploadRuleValidate: {
+        privacy: [
+          {
+            required: true,
+            message: "file privacy cannot be empty",
+            trigger: "blur"
+          }
+        ],
+        type: [
+          {
+            required: true,
+            message: "file type cannot be empty",
+            trigger: "blur"
+          }
+        ],
+        description: [
+          {
+            required: true,
+            message: "file description cannot be empty",
+            trigger: "blur"
+          }
+        ]
+      },
+      toUploadFiles: []
+    };
+  },
+  methods: {
+    getSubProjectInfo() {
+      this.axios
+        .get(
+          "/GeoProblemSolving/subProject/getFileStrcut" +
+            "?subProjectId=" +
+            this.subProjectId
+        )
+        .then(res => {
+          var fileStruct = res.data;
+          this.subProjectFileStruct = fileStruct;
+          this.currentFolder = fileStruct;
+          this.enterFolder(this.currentFolder);
+        })
+        .catch(err => {
+          console.log("Get sub-project's info fail.");
+        });
     },
-    data(){
-        return{
-            subProjectFileStruct:{},
-            subProjectId:"fbd438e0-b23b-49f6-93c8-36b678c70720",
-            currentFolder:{},
-            folderUIDStack:[],
-            newFolderModal:false,
-            setFolderName:"",
-            newValidate:{
-                setName:"",
-            },
-            newRuleValidate:{
-                setName: [
-                    {
-                        required: true,
-                        message: "The name can't be null.",
-                        trigger: "blur"
-                    }
-                ]
-            },
-            renameFolderInfo:{},
-            renameFolderModal:false,
-            renameValidate:{
-                newName:""
-            },
-            renameRuleValidate: {
-                newName: [
-                    {
-                        required: true,
-                        message: "The name can't be null.",
-                        trigger: "blur"
-                    }
-                ]
-            },
-        }
+    enterFolder(folder) {
+      this.folderUIDStack.push(this.currentFolder.uid);
+      this.currentFolder = folder;
     },
-    methods:{
-        getSubProjectInfo(){
-            this.axios
-            .get("/GeoProblemSolving/subProject/getFileStrcut"+"?subProjectId="+this.subProjectId)
-            .then(res=>{
-                var fileStruct = res.data;
-                this.subProjectFileStruct = fileStruct;
-                this.currentFolder = fileStruct;
-                this.enterFolder(this.currentFolder);
+    backforeFolder() {
+      if (this.folderUIDStack.length > 1) {
+        var foreFolderUid = this.folderUIDStack.pop();
+        this.refreshCurrentFolder(this.subProjectFileStruct, foreFolderUid);
+      } else {
+        this.$Message.warning("This is the root folder.");
+      }
+    },
+    getFileInfo(file) {
+      confirm(file.uid);
+    },
+    addFolderModalShow() {
+      this.newValidate.setName = "";
+      this.newFolderModal = true;
+    },
+    addFolder(name) {
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          var currentFolderUid = this.currentFolder.uid;
+          var subProjectId = this.subProjectId;
+          var newFolderName = this.newValidate.setName;
+          this.axios
+            .post(
+              "/GeoProblemSolving/subProject/createFolder" +
+                "?subProjectId=" +
+                subProjectId +
+                "&parentId=" +
+                currentFolderUid +
+                "&folderName=" +
+                newFolderName
+            )
+            .then(res => {
+              if (res.data != "Fail") {
+                this.subProjectFileStruct = res.data;
+                this.refreshCurrentFolder(res.data, this.currentFolder.uid);
+                this.newFolderModal = false;
+              } else {
+                console.log("New folder fail.");
+              }
             })
-            .catch(err=>{
-                console.log("Get sub-project's info fail.");
-            });
-        },
-        enterFolder(folder){
-            this.folderUIDStack.push(this.currentFolder.uid);
-            this.currentFolder = folder;
-        },
-        backforeFolder(){
-            if(this.folderUIDStack.length>1){
-                var foreFolderUid= this.folderUIDStack.pop();
-                this.refreshCurrentFolder(this.subProjectFileStruct,foreFolderUid);
-            }else{
-                this.$Message.warning("This is the root folder.");
-            }
-        },
-        getFileInfo(file){
-            confirm(file.uid);
-        },
-        addFolderModalShow(){
-            this.newValidate.setName = "";
-            this.newFolderModal = true;
-        },
-        addFolder(name){
-            this.$refs[name].validate(valid => {
-                if (valid) {
-                    var currentFolderUid = this.currentFolder.uid;
-                    var subProjectId = this.subProjectId;
-                    var newFolderName = this.newValidate.setName;
-                    this.axios
-                    .post("/GeoProblemSolving/subProject/createFolder"+
-                    "?subProjectId="+subProjectId+
-                    "&parentId="+currentFolderUid+
-                    "&folderName="+newFolderName)
-                    .then(res=>{
-                        if(res.data!="Fail"){
-                            this.subProjectFileStruct = res.data;
-                            this.refreshCurrentFolder(res.data,this.currentFolder.uid);
-                            this.newFolderModal =false;
-                        }else{
-                            console.log("New folder fail.");
-                        }
-                    })
-                    .catch(err=>{
-                        console.log("New folder fail.");
-                    });
-                }
-            });
-        },
-        refreshCurrentFolder(folder,uid){
-            if(folder.uid == uid){
-                this.currentFolder = Object.assign({},folder);
-                return;
-            }
-            else{
-                var subFolder = folder.folders;
-                for(let i=0;i<subFolder.length;i++){
-                    this.refreshCurrentFolder(subFolder[i],uid);
-                }
-            }
-        },
-        deleteFolder(folder){
-            if(confirm("Are you sure to delete this folder?")){
-                var currentFolderUid = this.currentFolder.uid;
-                var subProjectId = this.subProjectId;
-                var deleteFolderUid = folder.uid;
-                this.axios
-                .post("/GeoProblemSolving/subProject/deleteFolder"+
-                "?subProjectId="+subProjectId+
-                "&parentId="+currentFolderUid+
-                "&folderUid="+deleteFolderUid)
-                .then(res=>{
-                    if(res.data!="Fail"){
-                        this.subProjectFileStruct = res.data;
-                        this.refreshCurrentFolder(res.data,this.currentFolder.uid);
-                    }else{
-                        console.log("Delete folder fail.");
-                    }
-                })
-                .catch(err=>{
-                    console.log("Delete folder fail.");
-                });
-            }
-        },
-        renameFolderModalShow(folder){
-            this.renameFolderInfo = folder;
-            this.renameValidate.newName = "";
-            this.renameFolderModal = true;
-        },
-        renameFolder(name){
-            this.$refs[name].validate(valid => {
-                if (valid) {
-                    var currentFolderUid = this.currentFolder.uid;
-                    var subProjectId = this.subProjectId;
-                    var oldFolderInfo = this.renameFolderInfo;
-                    var newName = this.renameValidate.newName;
-                    this.axios
-                    .post("/GeoProblemSolving/subProject/renameFolder"+
-                    "?subProjectId="+subProjectId+
-                    "&parentId="+currentFolderUid+
-                    "&folderName="+newName+
-                    "&folderUid="+oldFolderInfo.uid)
-                    .then(res=>{
-                        if(res.data!="Fail"){
-                            this.subProjectFileStruct = res.data;
-                            this.refreshCurrentFolder(res.data,this.currentFolder.uid);
-                        }else{
-                            console.log("Rename fail.")
-                        }
-                    })
-                    .catch(err=>{console.log("Rename fail.")});
-                    this.renameFolderModal = false;
-                }
+            .catch(err => {
+              console.log("New folder fail.");
             });
         }
+      });
+    },
+    refreshCurrentFolder(folder, uid) {
+      if (folder.uid == uid) {
+        this.currentFolder = Object.assign({}, folder);
+        return;
+      } else {
+        var subFolder = folder.folders;
+        for (let i = 0; i < subFolder.length; i++) {
+          this.refreshCurrentFolder(subFolder[i], uid);
+        }
+      }
+    },
+    deleteFolder(folder) {
+      if (confirm("Are you sure to delete this folder?")) {
+        var currentFolderUid = this.currentFolder.uid;
+        var subProjectId = this.subProjectId;
+        var deleteFolderUid = folder.uid;
+        this.axios
+          .post(
+            "/GeoProblemSolving/subProject/deleteFolder" +
+              "?subProjectId=" +
+              subProjectId +
+              "&parentId=" +
+              currentFolderUid +
+              "&folderUid=" +
+              deleteFolderUid
+          )
+          .then(res => {
+            if (res.data != "Fail") {
+              this.subProjectFileStruct = res.data;
+              this.refreshCurrentFolder(res.data, this.currentFolder.uid);
+            } else {
+              console.log("Delete folder fail.");
+            }
+          })
+          .catch(err => {
+            console.log("Delete folder fail.");
+          });
+      }
+    },
+    renameFolderModalShow(folder) {
+      this.renameFolderInfo = folder;
+      this.renameValidate.newName = "";
+      this.renameFolderModal = true;
+    },
+    renameFolder(name) {
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          var currentFolderUid = this.currentFolder.uid;
+          var subProjectId = this.subProjectId;
+          var oldFolderInfo = this.renameFolderInfo;
+          var newName = this.renameValidate.newName;
+          this.axios
+            .post(
+              "/GeoProblemSolving/subProject/renameFolder" +
+                "?subProjectId=" +
+                subProjectId +
+                "&parentId=" +
+                currentFolderUid +
+                "&folderName=" +
+                newName +
+                "&folderUid=" +
+                oldFolderInfo.uid
+            )
+            .then(res => {
+              if (res.data != "Fail") {
+                this.subProjectFileStruct = res.data;
+                this.refreshCurrentFolder(res.data, this.currentFolder.uid);
+              } else {
+                console.log("Rename fail.");
+              }
+            })
+            .catch(err => {
+              console.log("Rename fail.");
+            });
+          this.renameFolderModal = false;
+        }
+      });
+    },
+    uploadModalShow() {
+      this.toUploadFiles = [];
+      this.uploadModal = true;
+    },
+    gatherFile(file) {
+      let that = this;
+      if (that.toUploadFiles.length >= 5) {
+        this.$Message.info("最多只能上传5个文件");
+      } else {
+        var fileSize = file.size;
+        if (fileSize < 1024) {
+          file.fileSize = fileSize + "b";
+        } else if (fileSize < 1024 * 1024) {
+          file.fileSize = Math.round((fileSize / 1024) * 100) / 100 + "Kb";
+        } else {
+          file.fileSize =
+            Math.round((fileSize / (1024 * 1024)) * 100) / 100 + "Mb";
+        }
+        that.toUploadFiles.push(file);
+      }
+      return false;
+    },
+    delFileList(index) {
+      this.toUploadFiles.splice(index, 1);
+    },
+    subProjectUpload(name) {
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          confirm("可以上传");
+        }
+      });
     }
-}
+  }
+};
 </script>
